e5a65026f0a341d42c0ffe8414817c5492bd40db
tom <chings228@gmail.com>
2023-09-05 11:25:00 +0800

    init

---
+++ ReadNumberGame/ReadNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadNumberGameApp.swift
+//  ReadNumberGame
+//
+//  Created by man ching chan on 5/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReadNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
